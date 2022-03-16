224df356b49f0b3ef78ff07913880bf8f9a147e0
MajaDit <marie.ditrtova@gmail.com>
2022-03-16 09:19:21 +0000

    debugging

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let lockBoard = false;
 
 function flipCard() {
   if (lockBoard) return;
+  if (this === firstCard) return;
 
   this.classList.toggle("flip");
 
@@ -31,6 +32,7 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+  resetBoard();
 }
 
 function unflipCards() {
@@ -40,7 +42,12 @@ function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    lockBoard = false;
+    resetBoard();
   }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 cards.forEach((card) => card.addEventListener("click", flipCard));
